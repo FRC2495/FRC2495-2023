73e957b91950e5d5c62ff9a83381608a1b90e580
Mr. A <GabrielArtaud@comcast.net>
2023-02-03 21:46:43 -0500

    Created baseline for Rotator

---
+++ src/main/java/frc/robot/interfaces/IRotator.java
@@ -0,0 +1,38 @@
+package frc.robot.interfaces;
+
+import edu.wpi.first.wpilibj.Joystick;
+
+public interface IRotator {
+	// returns the state of the limit switch
+	public boolean getLimitSwitchState();
+	
+	// This method should be called to assess the progress of a move
+	public boolean tripleCheckMove();
+
+	public void flip();
+
+	public void sideway();
+	
+	public void rest();
+	
+	public double getEncoderPosition();
+	
+	public boolean isMoving();
+	
+	public boolean isFlipping();	
+
+	// return if stalled
+	public boolean isStalled();
+
+	public void stay();	
+		
+	public void stop();
+
+	// NOTE THAT THIS METHOD WILL IMPACT BOTH OPEN AND CLOSED LOOP MODES
+	public void setNominalAndPeakOutputs(double peakOutput);
+
+	// for debug purpose only
+	public void joystickControl(Joystick joystick);
+	
+	public double getTarget();
+}
