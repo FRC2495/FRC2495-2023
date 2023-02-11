8bba22da58b7c7b798f2e09bb05cfaae52471b5e
frc2495 <frc2495@gmail.com>
2023-02-11 14:45:40 -0500

    changed CAN Id for REV PCM

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -120,7 +120,7 @@ public class Ports {
 			public static final int HINGE_MASTER = 6;
 			public static final int HINGE_FOLLOWER = 7;
 			//public static final int SPINNER = 8;
-			public static final int PCM = 9;
+			public static final int PCM = 1;
 			public static final int PDP = 0;	
 			public static final int ARM_MASTER = 10;
 			public static final int ARM_FOLLOWER = 11;
