b0d1ce745e84dd62bbbdd3658a4f44a4ee7a0498
ginaaaw <25giwong@gmail.com>
2023-02-19 21:31:54 -0500

    fixed shouldermovefloor stall detection command

--- src/main/java/frc/robot/commands/shoulder/ShoulderSafeMoveFloorWithStallDetection.java
+++ src/main/java/frc/robot/commands/shoulder/ShoulderSafeMoveFloorWithStallDetection.java
@@ -1,42 +1,18 @@
 
 package frc.robot.commands.shoulder;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 
-import frc.robot.Robot;
+import frc.robot.util.*;
+import frc.robot.commands.DoNothing;
 
 /**
  *
  */
-public class ShoulderSafeMoveFloorWithStallDetection extends CommandBase {
+public class ShoulderSafeMoveFloorWithStallDetection extends ConditionalCommand {
 
 	public ShoulderSafeMoveFloorWithStallDetection() {
-		addRequirements(Robot.shoulder);
+		super(new ShoulderMoveFloorWithStallDetection(), new DoNothing(), new ArmAndRotatorSafetyCheckForShoulderAtFloor());
 	}
 
-	// Called just before this Command runs the first time
-	@Override
-	public void initialize() {
-		System.out.println("ShoulderSafeMoveFloorWithStallDetection: initialize");
-		Robot.shoulder.moveUp();
-	}
-
-	// Called repeatedly when this Command is scheduled to run
-	@Override
-	public void execute() {
-		// nothing
-	}
-
-	// Make this return true when this Command no longer needs to run execute()
-	@Override
-	public boolean isFinished() {
-		return !Robot.shoulder.tripleCheckMove() || Robot.shoulder.tripleCheckIfStalled();
-	}
-
-	// Called once after isFinished returns true
-	@Override
-	public void end(boolean interupted) {
-		System.out.println("ShoulderSafeMoveFloorWithStallDetection: end");
-		Robot.shoulder.stay();  // we don't want to stop so we stay at the floor...
-	}
 }
