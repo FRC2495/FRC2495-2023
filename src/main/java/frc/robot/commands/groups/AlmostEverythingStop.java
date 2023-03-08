78fd674e747bef8833f584b1dcfb9d6805bde284
gartaud <GabrielArtaud@comcast.net>
2023-03-08 11:34:48 -0500

    also stops Jack

--- src/main/java/frc/robot/commands/groups/AlmostEverythingStop.java
+++ src/main/java/frc/robot/commands/groups/AlmostEverythingStop.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 //import frc.robot.commands.*;
 import frc.robot.commands.shoulder.*;
 import frc.robot.commands.arm.*;
+import frc.robot.commands.jack.*;
 import frc.robot.commands.rotator.*;
 //import frc.robot.commands.grasper.*;
 //import frc.robot.commands.feeder.*;
@@ -22,6 +23,7 @@ public class AlmostEverythingStop extends SequentialCommandGroup {
 		addCommands(
 			new ShoulderStop(),
 			new ArmStop(),
-			new RotatorStop());
+			new RotatorStop(),
+			new JackStop());
 	} 
 }
